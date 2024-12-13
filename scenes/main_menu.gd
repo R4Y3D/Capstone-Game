f3f08527ee07c4dab97a6a1c1413b33dd77ee995
Steven Uy <81347262+Steven-UY@users.noreply.github.com>
2024-12-13 13:27:28 -0500

    added tutorial and main menu

---
+++ scenes/main_menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+func on_play_game_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func on_tutorial_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
