ae5cda4875fdcf3c6eb76a26120435139565e8c0
Steven Uy <81347262+Steven-UY@users.noreply.github.com>
2024-12-13 13:35:56 -0500

    changed main scene and refined tutorial page

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -5,4 +5,4 @@ func on_play_game_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func on_tutorial_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
